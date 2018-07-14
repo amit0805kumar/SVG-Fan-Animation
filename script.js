72bdf5ff71ba7cb5bae1ebcd7f71131c8747db68
Amit Kumar <32258879+geekospot@users.noreply.github.com>
2018-07-14 10:44:19 +0530

    All files uploaded.

---
+++ script.js
@@ -0,0 +1,9 @@
+document.querySelector('.btn-start').addEventListener('click',function(){
+    document.querySelector('.st10').classList.add('wings');
+  
+});
+
+document.querySelector('.btn-stop').addEventListener('click',function(){
+    document.querySelector('.st10').classList.remove('wings');
+  
+});
